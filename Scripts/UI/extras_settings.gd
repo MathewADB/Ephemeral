46fb21479c8f9206c8b8ec595108ca709e0d4cc3
Black-Phoenix <Alirezaadab7@gmail.com>
2026-04-27 15:39:18 +0200

    1.1.3.1 alpha
    Tilesets rework
    main menu theme changed
    game theme changed
    backgrounds replaced
    main menu background added
    achievement progress now visible
    death count, level, playtime now visible in extras
    sprites improved

--- Scripts/UI/extras_settings.gd
+++ Scripts/UI/extras_settings.gd
@@ -3,13 +3,28 @@ extends Control
 @onready var container = $Panel/ScrollContainer/GridContainer
 var item_scene = preload("res://Scenes/Control/achievement_item.tscn")
 
+var max_achievements : int = 0
+var unlocked_achievements : int = 0
+@warning_ignore("narrowing_conversion")
+var minimum_space : int = 10.0
+
 func _ready():
 	$Panel/Close.text = tr("CLOSE")
+	
 	for id in Manager.ACHIEVEMENT_DATA.keys():
+		minimum_space += 100
+		max_achievements += 1
+		if Manager.achievements[id].unlocked == true :
+			unlocked_achievements += 1
 		var item = item_scene.instantiate()
 		container.add_child(item)
 		item.setup(id)
-
-
+	
+	container.set_custom_minimum_size(Vector2(0.0,minimum_space))
+	$Panel/Amount.text = (str(unlocked_achievements)+"/"+str(max_achievements))
+	$Panel/Deaths.text = str(Manager.death_count) + " Deaths"
+	$Panel/Playtime.text = str(int(Manager.game_seconds) -433) + " Seconds"
+	$Panel/Level.text = "Level : " + str(Manager.level)
+	
 func _on_close_pressed() -> void:
 	self.visible = false
